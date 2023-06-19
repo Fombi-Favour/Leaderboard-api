520f2c3498823cc17771ccff161e46c2b2183923
Fombi-Favour <fombifavour@gmail.com>
2023-06-19 19:42:57 +0100

    Update listStorage.js

--- src/listStorage.js
+++ src/listStorage.js
@@ -1,18 +1,20 @@
 const scoreList = [
-    {name: 'john', score: 100},
-    {name: 'jane', score: 200},
-    {name: 'jack', score: 300},
-    {name: 'jill', score: 400},
-    {name:'mary', score: 500},
-    {name: 'peter', score: 600},
-    {name: 'peter', score: 700},
-    {name: 'peter', score: 800},
-    {name: 'peter', score: 900},
+  { name: 'john', score: 100 },
+  { name: 'jane', score: 200 },
+  { name: 'jack', score: 300 },
+  { name: 'jill', score: 400 },
+  { name: 'mary', score: 500 },
+  { name: 'peter', score: 600 },
+  { name: 'peter', score: 700 },
+  { name: 'peter', score: 800 },
+  { name: 'peter', score: 900 },
 ];
 
+const recentList = document.querySelector('.recent-list');
+
 const displayScore = () => {
-    scoreList.forEach((score) => {
-        recentList.innerHTML += `<li class="list-item">${score.name}: ${score.score}</li>`;
-    });
+  scoreList.forEach((score) => {
+    recentList.innerHTML += `<li class="list-item">${score.name}: ${score.score}</li>`;
+  });
 };
 exports.displayScore = displayScore;
