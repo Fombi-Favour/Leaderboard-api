efeeb348389dcf440eb7e9187fde749cd4335ea8
Fombi-Favour <fombifavour@gmail.com>
2023-06-20 13:34:51 +0100

    Update listStorage.js

--- src/listStorage.js
+++ src/listStorage.js
@@ -1,21 +1,48 @@
-const scoreList = [
-  { name: 'john', score: 100 },
-  { name: 'jane', score: 200 },
-  { name: 'jack', score: 300 },
-  { name: 'jill', score: 400 },
-  { name: 'mary', score: 500 },
-  { name: 'peter', score: 600 },
-  { name: 'peter', score: 700 },
-  { name: 'peter', score: 800 },
-  { name: 'peter', score: 900 },
-];
-
 const recentList = document.querySelector('.recent-list');
+const apiBaseURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
+const idURL = 'Zl4d7IVkemOTTVg2fUdz/';
 
-const displayScore = () => {
-  scoreList.forEach((score) => {
-    recentList.innerHTML += `<li class="list-item">${score.name}: ${score.score}</li>`;
-  });
+const scoreList = [];
+
+const getScore = async () => {
+  const response = await fetch(`${apiBaseURL}${idURL}scores/`)
+    .then((res) => res.json)
+    .then((data) => data.result)
+    .catch(() => 'error');
+  return response;
 };
 
-exports.displayScore = displayScore;
+const addScore = async (data) => {
+  const response = await fetch(`${apiBaseURL}${idURL}scores/`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+
+  const res = await response.json();
+  return res;
+};
+
+exports.addScore = addScore;
+
+// const scoreList = [
+//   { name: 'john', score: 100 },
+//   { name: 'jane', score: 200 },
+//   { name: 'jack', score: 300 },
+//   { name: 'jill', score: 400 },
+//   { name: 'mary', score: 500 },
+//   { name: 'peter', score: 600 },
+//   { name: 'peter', score: 700 },
+//   { name: 'peter', score: 800 },
+//   { name: 'peter', score: 900 },
+// ];
+
+// const displayScore = () => {
+//   scoreList.forEach((score) => {
+//     recentList.innerHTML += `<li class="list-item">${score.name}: ${score.score}</li>`;
+//   });
+// };
+
+// exports.displayScore = displayScore;
