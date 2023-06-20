2b952b58443945e2cab0123175a8f91ed3ffd9de
Fombi-Favour <fombifavour@gmail.com>
2023-06-20 13:34:44 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,28 @@
-import { displayScore } from './listStorage.js';
+import { displayScore, addScore } from './listStorage.js';
 import './style.css';
 
-displayScore();
+const forms = document.getElementById('forms');
+// const refresh = document.getElementById("refresh");
+
+// refresh.addEventListener("click", () => {
+//   displayScore();
+// });
+
+forms.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const name = document.getElementById('name').value;
+  const score = document.getElementById('score').value;
+  if (name !== '' && score !== '') {
+    const lists = {
+      name,
+      score,
+    };
+    await addScore(lists);
+    console.log('Added!!!');
+  }
+
+  document.getElementById('name').value = '';
+  document.getElementById('score').value = '';
+
+//   displayScore();
+});
