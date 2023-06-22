93c17a3ff1c4fedadfec7aed66e6c3f0e6aabcee
Fombi-Favour <fombifavour@gmail.com>
2023-06-22 14:01:02 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,2 @@
-import { displayScore, addScore } from './listStorage.js';
+import './listStorage.js';
 import './style.css';
-
-const forms = document.querySelector('#forms');
-const refresh = document.getElementById('refresh');
-
-refresh.addEventListener('click', () => {
-  displayScore();
-});
-
-forms.addEventListener('submit', async (e) => {
-  e.preventDefault();
-  const userName = document.getElementById('name').value;
-  const userScore = document.getElementById('score').value;
-  if (userName !== '' && userScore !== '') {
-    await addScore(userName, userScore);
-    // console.log('Added!!!');
-    document.getElementById('name').value = '';
-    document.getElementById('score').value = '';
-  }
-
-  displayScore();
-});
